8a4aa76a66a9cf83facc71526b5329da7db68e6c
Casoteva <casoteva@gmail.com>
2025-04-15 11:14:04 -0600

    Add files via upload

---
+++ addons/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Cleanup Attachment",
+    "version": "1.0",
+    "category": "Tools",
+    "summary": "Limpia attachments huérfanos sin archivo físico",
+    "author": "Tú",
+    "depends": ["base"],
+    "installable": true,
+    "application": false
+}
